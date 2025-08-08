9e0f205540fbeca5cdd1d859d54bf40ec9db02e9
ahmed <20ahmdsaad24@gmail.com>
2025-08-08 05:00:16 +0300

    shared pref for one time appear for the intro screen

--- lib/ui/utils/app_images.dart
+++ lib/ui/utils/app_images.dart
@@ -1,5 +1,6 @@
 class AppImages{
   static const String nativeScreenImage = 'asset/images/splash_screen_images.png';
+  static const String logInImage = 'asset/images/evently_log_in_image.png';
   static const String splashScreenBranding = 'asset/images/splash_screen_branding.png';
   static const String eventlyLogo = 'asset/images/evently_logo.png';
   static const String personalizeScreenImage = 'asset/images/personalize_screen_image.png';
