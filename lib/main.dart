7e9cc7e989e0094a6134f7aa2d05bb79efef49ac
ahmed <20ahmdsaad24@gmail.com>
2025-08-08 05:00:16 +0300

    shared pref for one time appear for the intro screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,31 @@
+import 'package:evently/firebase_services/fcm.dart';
 import 'package:evently/ui/screens/home_screen/home_screen.dart';
 import 'package:evently/ui/screens/home_screen/intro_screen_shred_pref.dart';
+import 'package:evently/ui/screens/log_in_screen/log_screen.dart';
 import 'package:evently/ui/screens/on_boarding_screen/customize_first_screen.dart';
 import 'package:evently/ui/screens/on_boarding_screen/on_boarding_screen.dart';
 import 'package:evently/ui/utils/app_theme.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'firebase_options.dart';
+
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+
+  print("Handling a background message: ${message.messageId}");
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await FcmServices.init();
+  await FcmServices.getToken();;
+
   final isIntroScreenOpened = await IntroScreenShredPref.isIntroScreenOpened();
   runApp(MyApp(isIntroScreenOpened: isIntroScreenOpened));
 }
@@ -33,12 +51,13 @@ class MyApp extends StatelessWidget {
           theme: AppTheme.lightMode,
           darkTheme: AppTheme.darkMode,
           debugShowCheckedModeBanner: false,
-          initialRoute:
-              isIntroScreenOpened ? "/personalizeScreen" : "/homeScreen",
+          initialRoute: isIntroScreenOpened ?
+          "/personalizeScreen" : "/logInScreen",
           routes: {
             '/homeScreen': (_) => HomeScreen(),
             "/personalizeScreen": (_) => CustomizeFirstScreen(),
             "/OnBoardingScreen": (_) => OnBoardingScreen(),
+            "/logInScreen": (_) => LogScreen(),
           },
         );
       },
--- lib/ui/screens/on_boarding_screen/customize_first_screen.dart
+++ lib/ui/screens/on_boarding_screen/customize_first_screen.dart
@@ -3,23 +3,33 @@ import 'package:evently/ui/utils/app_colors.dart';
 import 'package:evently/ui/utils/app_fonts.dart';
 import 'package:evently/ui/utils/app_icons.dart';
 import 'package:evently/ui/utils/app_images.dart';
+import 'package:evently/ui/widget/select_button_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class CustomizeFirstScreen extends StatelessWidget {
+class CustomizeFirstScreen extends StatefulWidget {
   static const String routeName = '/personalizeScreen';
 
   const CustomizeFirstScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  State<CustomizeFirstScreen> createState() => _CustomizeFirstScreenState();
+}
 
+class _CustomizeFirstScreenState extends State<CustomizeFirstScreen> {
+  String languageButton = '';
+  String moodButton = '';
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.whiteBG,
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 20.0.w),
-          child: Column(spacing: 28.h,mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          child: Column(
+            spacing: 28.h,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Center(child: Image.asset(AppImages.eventlyLogo)),
               Image.asset(AppImages.personalizeScreenImage),
@@ -39,24 +49,11 @@ class CustomizeFirstScreen extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Text('Language', style: AppFonts.lightBlue20inter500),
-                  Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(color: AppColors.lightBlue, width: 3),
-                      borderRadius: BorderRadius.all(Radius.circular(30)),
-                    ),
-                    child: Row(
-                      spacing: 17,
-                      children: [
-                        GestureDetector(
-                          onTap: () {},
-                          child: Image.asset(AppICons.americaFlag),
-                        ),
-                        GestureDetector(
-                          onTap: () {},
-                          child: Image.asset(AppICons.egFlag),
-                        ),
-                      ],
-                    ),
+                  SelectButtonWidget(
+                    buttonKey1: 'en',
+                    image1: AppICons.americaFlag,
+                    image2: AppICons.egFlag,
+                    buttonKey2: 'ar',
                   ),
                 ],
               ),
@@ -64,18 +61,14 @@ class CustomizeFirstScreen extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Text('Theme', style: AppFonts.lightBlue20inter500),
-                  Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(color: AppColors.lightBlue, width: 3),
-                      borderRadius: BorderRadius.all(Radius.circular(30)),
-                    ),
-                    child: Row(
-                      spacing: 17,
-                      children: [
-                        Image.asset(AppICons.sunIcon, color: Color(0xffffa000)),
-                        Image.asset(AppICons.moonIcon),
-                      ],
-                    ),
+                  SelectButtonWidget(
+                    buttonKey1: 'sun',
+                    image1: AppICons.sunIcon,
+                    selectColorImage1: AppColors.white,
+                    unSelectSecondColorImage1: Color(0xffff8f00),
+                    image2: AppICons.moonIcon,
+                    buttonKey2: 'moon',
+                    selectColorImage2: AppColors.white,
                   ),
                 ],
               ),
